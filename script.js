8074b609a1f89ea6d573cf31200cc735b55a3c2b
naveen15597 <63700657+naveen15597@users.noreply.github.com>
2021-09-12 18:44:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+
+setTimeout(()=>{
+    document.getElementsByName("para")[0].innerText = 10;
+    setTimeout(()=>{
+        document.getElementsByName("para")[0].innerText = 9;
+        setTimeout(()=>{
+            document.getElementsByName("para")[0].innerText = 8;
+            setTimeout(()=>{
+                document.getElementsByName("para")[0].innerText = 7;
+                setTimeout(()=>{
+                    document.getElementsByName("para")[0].innerText = 6;
+                    setTimeout(()=>{
+                        document.getElementsByName("para")[0].innerText = 5;
+                        setTimeout(()=>{
+                            document.getElementsByName("para")[0].innerText = 4;
+                            setTimeout(()=>{
+                                document.getElementsByName("para")[0].innerText = 3;
+                                setTimeout(()=>{
+                                    document.getElementsByName("para")[0].innerText = 2;
+                                    setTimeout(()=>{
+                                        document.getElementsByName("para")[0].innerText = 1;
+                                        setTimeout(()=>{
+                                            document.getElementsByName("para")[0].innerText = "Happy Independence Day";
+                                          },1000)
+                                      },1000)
+                                  },1000)
+                              },1000)
+                          },1000)
+                      },1000)
+                  },1000)
+              },1000)
+          },1000)
+      },1000)    
+  },1000)
